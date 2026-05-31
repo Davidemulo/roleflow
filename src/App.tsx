ee6004ce991f4d280071508ff2e84e289556adfd
Davidemulo <emulodavid@gmail.com>
2026-05-31 13:11:11 +0100

    Setup routing and base pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
+import NotFound from "./pages/NotFound";
+
 function App() {
   return (
-    <div>
-      <h1>RoleFlow App Starting...</h1>
-    </div>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
